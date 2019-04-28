0c611d62606aced05c046107cab0eeb9b27e794e
Conor Souhrada <ConorSouhrada@gmail.com>
2019-04-28 08:09:28 -0600

    testing set up

---
+++ src/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import App from './App';
+import { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<App />', () => {
+  it('renders one <App /> component', () => {
+    const component = shallow(<App />)
+    expect(component).toHaveLength(1)
+  })
+});
